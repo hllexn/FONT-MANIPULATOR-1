d8db985f26d0fe1ad0a48b5255a9e0822580b415
hllexn <sugandhijaiswal2008@gmail.com>
2024-05-28 14:08:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+   
+
+    canvas=createCanvas(550,550);
+    
+    
+
+
+}
+function draw(){
+    
+
+}
